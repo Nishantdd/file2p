b235dcfdde0d28d6bc1a4a6be4eb39b1c1f52a8f
Nishant <nishant.dahiya2000@gmail.com>
2026-03-03 23:08:56 +0530

    add bun ws example

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,13 @@
-import { WebSocket } from "bun";
+const server = Bun.serve({
+  fetch(req, server) {
+    if (server.upgrade(req)) return;
+    return new Response("Upgrade failed", { status: 500 });
+  },
+  websocket: {
+    message(ws, message) {
+      ws.send(message);
+    },
+  },
+});
 
-Bun.serve();
+console.log(`server running at ${server.url}`);
