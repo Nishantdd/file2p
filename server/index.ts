057211d1ec84cf86c28c3504527f2dd31b202c57
Nishantdd <nishant.dahiya2000@gmail.com>
2026-03-04 22:59:44 +0530

    store host socket connections

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,46 @@
+type SocketData = {
+  peerId: string;
+  transferId: string;
+  role: "host" | "leecher";
+};
+
+const transferMap = new Map<string, Bun.ServerWebSocket<SocketData>>();
+
 const server = Bun.serve({
+  port: 8000,
   fetch(req, server) {
-    if (server.upgrade(req)) return;
+    const url = new URL(req.url);
+    const transferId = url.searchParams.get("transferId")!;
+    const peerId = crypto.randomUUID();
+    const role = (url.searchParams.get("role") || "leecher") as
+      | "host"
+      | "leecher";
+
+    if (server.upgrade(req, { data: { peerId, transferId, role } })) return;
     return new Response("Upgrade failed", { status: 500 });
   },
   websocket: {
-    message(ws, message) {
-      ws.send(message);
+    data: {} as SocketData,
+
+    open(ws) {
+      if (ws.data.role === "host") {
+        transferMap.set(ws.data.transferId, ws);
+      } else {
+        const hostWebSocket = transferMap.get(ws.data.transferId)!;
+        hostWebSocket.send(
+          JSON.stringify({ type: "message", from: ws.data.peerId }),
+        );
+      }
+    },
+
+    message(ws, raw) {
+      const msg = JSON.parse(raw as string);
+    },
+
+    close(ws) {
+      transferMap.delete(ws.data.transferId);
     },
   },
 });
 
-console.log(`server running at ${server.url}`);
+console.log(`Signalling server running at ${server.url}`);
