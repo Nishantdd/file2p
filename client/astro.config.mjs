7ff7f85603735010d73b21966599944567e71789
Nishantdd <nishant.dahiya2000@gmail.com>
2026-03-05 16:05:26 +0530

    add preact integration

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [preact()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
