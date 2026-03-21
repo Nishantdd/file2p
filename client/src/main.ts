bba5d07c28624ae9968e9448670142ed01ef755b
Nishantdd <nishant.dahiya2000@gmail.com>
2026-03-21 18:23:38 +0530

    add prettier to client

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,18 +1,16 @@
-const tabButtons = document.querySelectorAll<HTMLButtonElement>(".tab-btn");
-const contentSend = document.getElementById("content-send")!;
-const contentReceive = document.getElementById("content-receive")!;
+const tabButtons = document.querySelectorAll<HTMLButtonElement>('.tab-btn');
+const contentSend = document.getElementById('content-send')!;
+const contentReceive = document.getElementById('content-receive')!;
 
-type SendStates = "upload" | "ready";
-type ReceiveStates = "input" | "conn" | "cerr" | "ready" | "dl" | "derr";
+type SendStates = 'upload' | 'ready';
+type ReceiveStates = 'input' | 'conn' | 'cerr' | 'ready' | 'dl' | 'derr';
 
-tabButtons.forEach((btn) =>
-  btn.addEventListener("click", () => {
-    tabButtons.forEach((b) => (b.dataset.active = "false"));
-    btn.dataset.active = "true";
-    contentSend.dataset.active = "false";
-    contentReceive.dataset.active = "false";
-    btn.dataset.tab === "send"
-      ? (contentSend.dataset.active = "true")
-      : (contentReceive.dataset.active = "true");
-  }),
+tabButtons.forEach(btn =>
+  btn.addEventListener('click', () => {
+    tabButtons.forEach(b => (b.dataset.active = 'false'));
+    btn.dataset.active = 'true';
+    contentSend.dataset.active = 'false';
+    contentReceive.dataset.active = 'false';
+    btn.dataset.tab === 'send' ? (contentSend.dataset.active = 'true') : (contentReceive.dataset.active = 'true');
+  })
 );
