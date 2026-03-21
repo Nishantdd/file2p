78163d7987de33a242c92e49da225c7c9affe21a
Nishantdd <nishant.dahiya2000@gmail.com>
2026-03-21 22:44:03 +0530

    add a way to manipulate states and tabs

---
+++ client/src/switch-helpers.ts
@@ -0,0 +1,16 @@
+import type { TabStates, ReceiveStates, SendStates } from "./types/states.types";
+
+export const switchTabs = (
+  tabs: NodeListOf<HTMLButtonElement>,
+  panels: NodeListOf<HTMLDivElement>,
+  targetTab: TabStates
+) => {
+  tabs.forEach(tab => (tab.dataset.active = String(targetTab === tab.dataset.tab)));
+  panels.forEach(panel => (panel.dataset.active = String(targetTab === panel.dataset.panel)));
+};
+
+export const switchSendStates = (sendStates: NodeListOf<HTMLDivElement>, targetState: SendStates) =>
+  sendStates.forEach(state => (state.dataset.active = String(targetState === state.dataset.state)));
+
+export const switchReceiveStates = (receiveStates: NodeListOf<HTMLDivElement>, targetState: ReceiveStates) =>
+  receiveStates.forEach(state => (state.dataset.active = String(targetState === state.dataset.state)));
