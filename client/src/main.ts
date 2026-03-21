751a18e34c1c2468f85db6f3c68ee61b31276835
Nishantdd <nishant.dahiya2000@gmail.com>
2026-03-21 15:29:09 +0530

    create receive input state along with popin animation

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,14 +2,16 @@ const tabButtons = document.querySelectorAll<HTMLButtonElement>(".tab-btn");
 const contentSend = document.getElementById("content-send")!;
 const contentReceive = document.getElementById("content-receive")!;
 
+type SendStates = "upload" | "ready";
+type ReceiveStates = "input" | "conn" | "cerr" | "ready" | "dl" | "derr";
+
 tabButtons.forEach((btn) =>
   btn.addEventListener("click", () => {
     tabButtons.forEach((b) => (b.dataset.active = "false"));
-    const tab = btn.dataset.tab;
     btn.dataset.active = "true";
     contentSend.dataset.active = "false";
     contentReceive.dataset.active = "false";
-    tab === "send"
+    btn.dataset.tab === "send"
       ? (contentSend.dataset.active = "true")
       : (contentReceive.dataset.active = "true");
   }),
