4c090b024d2fc627c31f74058fd11456d1f48782
Nishantdd <nishant.dahiya2000@gmail.com>
2026-05-17 22:00:59 +0530

    add help.html in rollup input options as public file won't have access to css

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [tailwindcss()]
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        help: resolve(__dirname, "help.html")
+      }
+    }
+  }
 });
